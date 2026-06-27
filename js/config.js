bc93bed971b0debcd3c99ec905e9d28618cd8575
rahulmcse-dot <rahulmcse@gmail.com>
2026-06-27 13:02:03 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
+// Supabase Configuration
 
+const SUPABASE_URL = "https://ykcbdmdqzeonodemcavc.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_yhzQvFqLaMw861azeWeBSQ_iDUy70Jg";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
